991fdc781159554b2a71382ccef460a7900011f4
Bert Ciccone <bertfciccone@gmail.com>
2016-09-15 08:03:32 -0700

    refactor: improved bootstrap layout

--- js/helper.js
+++ js/helper.js
@@ -20,72 +20,106 @@ var HTMLrow = 'row';
 var HTMLtabs = '<div id="tabs"></div>';
 var HTMLtabList = '<ul class="tabList"></ul>';
 // var HTMLnavCol = '<div class="navCol col-xs-3"></div>';
-var HTMLnavProfile = '<li><div class="navCol col-xs-3"><a href="#headerCol" class="navItem fa fa-user fa-2x bg-user" aria-hidden="true"></a></div></li>';
-var HTMLnavWork = '<li><div class="navCol col-xs-3"><a href="#workCol" class="navItem fa fa-briefcase fa-2x bg-briefcase" aria-hidden="true"></a></div></li>';
-var HTMLnavEducation = '<li><div class="navCol col-xs-3"><a href="#educationCol" class="navItem fa fa-graduation-cap fa-2x bg-graduation-cap" aria-hidden="true"></a></div></li>';
-var HTMLnavContact = '<li><div class="navCol col-xs-3"><a href="#contactsCol" class="navItem fa fa-envelope fa-2x bg-envelope" aria-hidden="true"></a></div></li>';
+var HTMLnavProfile = '<li><div class="navCol col-xs-3"><a href="#bioCol" class="navItem fa fa-user fa-2x bg-user" aria-hidden="true"></a></div></li>';
+var HTMLnavWork = '<li><div class="navCol col-xs-3"><a href="#workHeadingCol" class="navItem fa fa-briefcase fa-2x bg-briefcase" aria-hidden="true"></a></div></li>';
+var HTMLnavEducation = '<li><div class="navCol col-xs-3"><a href="#educationHeadingCol" class="navItem fa fa-graduation-cap fa-2x bg-graduation-cap" aria-hidden="true"></a></div></li>';
+var HTMLnavContact = '<li><div class="navCol col-xs-3"><a href="#contactHeadingCol" class="navItem fa fa-envelope fa-2x bg-envelope" aria-hidden="true"></a></div></li>';
 
 // Header helper variables
-var HTMLheaderCol = '<div id="headerCol" class="col-xs-12"></div>';
-var HTMLbioPic = '<a href="#"><img src="%data%" class="biopic responsive-image center-block"></a>';
+var HTMLpicCol = '<div id="picCol" class="col-xs-12 col-sm-4"></div>';
+var HTMLbioCol = '<div id="bioCol" class="col-xs-12 col-sm-8"></div>';
+var HTMLbioPic = '<a href="#"><img src="%data%" class="biopic img-responsive center-block"></a>';
 var HTMLheaderName = '<h1 id="name"><a href="#">%data%</a></h1>';
 var HTMLheaderRole = '<p class="role-entry">%data%</p>';
 var HTMLwelcomeMsg = '<p class="welcome-message">%data%</p>';
 var HTMLrule = '<hr>';
 
 // Skills helper variables
-var HTMLskillsCol = '<div id="skillsCol" class="col-xs-12"><h2><a href="#">Skills at a Glance</a></h2></div>';
+var HTMLskillsHeadingCol = '<div id="skillsHeadingCol" class="col-xs-12"><h2><a href="#">Skills at a Glance</a></h2></div>';
+var HTMLskillsCol1 = '<div id="skillsCol1" class="col-xs-12 col-sm-4 col-sm-offset-4"></div>';
+var HTMLskillsCol2 = '<div id="skillsCol2" class="col-xs-12 col-sm-4"></div>';
 var HTMLskill = '<p class="skills-entry">%data%</p>';
 
 // Work helper variables
-var HTMLworkCol = '<div id="workCol" class="col-xs-12"><h2><a href="#">Work Experience</a></h2></div>';
+var HTMLworkHeadingCol = '<div id="workHeadingCol" class="col-xs-12"><h2><a href="#">Work Experience</a></h2></div>';
 var HTMLworkStart = '<div class="work-entry"></div>';
-var HTMLworkIcon = '<span class = "fa fa-briefcase fa-2x entry-icon"></span>'
-var HTMLworkEmployer = '<a href="#">%data%</a>';
-var HTMLworkTitle = '<a href="#">%data%</a>';
+var HTMLworkDatesCol = '<div class="workDatesCol col-xs-12 col-sm-3"></div>';
+var HTMLworkIconCol = '<div class="workIconCol col-xs-12 col-sm-1"></div>';
+var HTMLworkTitleCol = '<div class="workTitleCol col-xs-12 col-sm-4"></div>';
+var HTMLworkCompanyCol = '<div class="workCompanyCol col-xs-12 col-sm-4"></div>';
+var HTMLworkDescriptionCol = '<div class="workDescriptionCol col-xs-12 col-sm-8 col-sm-offset-4"></div>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
+var HTMLworkIcon = '<span class = "fa fa-briefcase fa-2x entry-icon"></span>'
+var HTMLworkTitle = '<a href="#">%data%</a>';
+var HTMLworkEmployer = '<a href="#">%data%</a>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
 var HTMLworkDescription = '<p><br>%data%</p>';
 
 // Projects helper variables
-var HTMLprojectCol = '<div id="projectCol" class="col-xs-12"><h2><a href="#">Projects</a></h2></div>';
+var HTMLprojectHeadingCol = '<div id="projectCol" class="col-xs-12"><h2><a href="#">Projects</a></h2></div>';
 var HTMLprojectStart = '<div class="project-entry"></div>';
-var HTMLprojectTitle = '<a href="#">%data%</a>';
+var HTMLprojectDateCol = '<div class="projectDateCol col-xs-12 col-sm-3"></div>';
+var HTMLprojectIconCol = '<div class="projectIconCol col-xs-12 col-sm-1"></div>';
+var HTMLprojectTitleCol = '<div class="projectTitleCol col-xs-12 col-sm-4"></div>';
+var HTMLprojectDescriptionCol = '<div class="projectDescriptionCol col-xs-12 col-sm-8 col-sm-offset-4"></div>';
+var HTMLprojectPicturesCol = '<div class="projectPicturesCol col-xs-12 col-sm-8 col-sm-offset-4"></div>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
+var HTMLprojectIcon = '<span class = "fa fa-folder fa-2x entry-icon"></span>'
+var HTMLprojectTitle = '<a href="#">%data%</a>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
 
 // Education helper variables
-var HTMLschoolsCol = '<div id="educationCol" class="col-xs-12"><h2><a href="#">Education</a></h2></div>';
+var HTMLschoolsHeadingCol = '<div id="educationHeadingCol" class="col-xs-12"><h2><a href="#">Education</a></h2></div>';
 var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
-var HTMLschoolDegree = '%data%</a>';
+var HTMLschoolDatesCol = '<div class="schoolDatesCol col-xs-12 col-sm-3"></div>';
+var HTMLschoolIconCol = '<div class="schoolIconCol col-xs-12 col-sm-1"></div>';
+var HTMLschoolNameCol = '<div class="schoolNameCol col-xs-12 col-sm-4"></div>';
+var HTMLschoolLocationCol = '<div class="schoolLocationCol col-xs-12 col-sm-4"></div>';
+var HTMLschoolDegreeCol = '<div class="schoolDegreeCol col-xs-12 col-sm-8 col-sm-offset-4"></div>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
+var HTMLschoolIcon = '<span class = "fa fa-graduation-cap fa-2x entry-icon"></span>'
+var HTMLschoolName = '<a href="#">%data%</a>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
+var HTMLschoolURL = '<br><a href="#">%data%</a>';
+var HTMLschoolDegree = '<a>%data%</a>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
 // Online courses helper variables
-var HTMLonlineClasses = '<h3><a href="#">Online Classes</a></h3>';
-var HTMLonlineTitle = '<a href="#">%data%</a>';
-var HTMLonlineSchool = '<a href="#">%data%</a>';
-var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<br><a href="#">%data%</a>';
+var HTMLcoursesHeadingCol = '<div id="coursesHeadingCol" class="col-xs-12 col sm-12"><h2><a href="#">Online Classes</a></h2></div>';
+var HTMLcourseStart = '<div class="course-entry"></div>';
+var HTMLcourseDatesCol = '<div class="courseDatesCol col-xs-12 col-sm-3"></div>';
+var HTMLcourseIconCol = '<div class="courseIconCol col-xs-12 col-sm-1"></div>';
+var HTMLcourseTitleCol = '<div class="courseTitleCol col-xs-12 col-sm-4"></div>';
+var HTMLcourseSchoolCol = '<div class="courseSchoolCol col-xs-12 col-sm-4"></div>';
+var HTMLcourseDates = '<div class="date-text">%data%</div>';
+var HTMLcourseIcon = '<span class = "fa fa-book fa-2x entry-icon"></span>'
+var HTMLcourseTitle = '<a href="#">%data%</a>';
+var HTMLcourseSchool = '<a href="#">%data%</a>';
+var HTMLcourseURL = '<br><a href="#">%data%</a>';
 
-var HTMLcontactsCol = '<div id="contactsCol" class="col-xs-12"><h2><a href="#">Contacts</a></h2></div>';
-var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="orange-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="orange-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="orange-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="orange-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="orange-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="orange-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="orange-text">%data%</span></li>';
+// Contact helper variables
+var HTMLcontactHeadingCol = '<div id="contactHeadingCol" class="col-xs-12"><h2><a href="#">Contact</a></h2></div>';
+var HTMLcontactRow = '<div class="row contact-row"</div>';
+var HTMLcontactStart = '<div class="contact-entry"></div>';
+var HTMLcontactBlankCol = '<div class="contact-blank-col col-xs-12 col-sm-3"></div>';
+var HTMLcontactIconCol = '<div class="contact-icon-col col-xs-12 col-sm-1"></div>';
+var HTMLcontactEntryCol = '<div class="contact-entry-col col-xs-12 col-sm-3"></div>';
+var HTMLmobileIcon = '<span class = "fa fa-mobile fa-2x entry-icon"></span>'
+var HTMLemailIcon = '<span class = "fa fa-envelope fa-2x entry-icon"></span>'
+var HTMLtwitterIcon = '<span class = "fa fa-twitter fa-2x entry-icon"></span>'
+var HTMLgithubIcon = '<span class = "fa fa-github fa-2x entry-icon"></span>'
+var HTMLblogIcon = '<span class = "fa fa-external-link-square fa-2x entry-icon"></span>'
+var HTMLlocationIcon = '<span class = "fa fa-map-marker fa-2x entry-icon"></span>'
+var HTMLcontact = '<div class="entry-info">%data%</div>';
 
 // Map helper variables
 var HTMLmapCol = '<div id="mapDiv" class="col-xs-12"></div>';
 var HTMLmapHeading = '<h2><a href="#">Places Where I\'ve Lived and Worked</a></h2>';
 var googleMap = '<div id="map"></div>';
 
-var internationalizeButton = '<button>Internationalize</button>';
+var HTMLintlCol = '<div id="intlCol" class="col-xs-12"></div>';
+var intlButton = '<button>Internationalize</button>';
 
 /*
 The Internationalize Names challenge found in the lesson Flow Control from JavaScript Basics requires you to create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,6 +11,7 @@ var bio = {
     "email": "bertfciccone@gmail.com",
     "github": "https://github.com/bertcuda",
     "twitter": "@bertctweets",
+    "blog": "http://bertciccone.com",
     "location": "San Francisco"
   },
   "welcomeMessage": "Leading successful launches of innovative technologies and products.",
@@ -66,7 +67,7 @@ var projects = {
       "http://placehold.it/200x200"
     ]
   }]
-}
+};
 
 console.log(projects);
 
@@ -77,14 +78,14 @@ var education = {
     "location": "Los Angeles, California",
     "degree": "MBA",
     "majors": ["Information Systems", "Entrepreneurship"],
-    "dates": "",
+    "dates": "", // intentionally left blank
     "url": "http://usc.edu"
   }, {
     "name": "California State University",
     "location": "Northridge, California",
     "degree": "BS",
     "majors": ["Computer Science"],
-    "dates": "",
+    "dates": "", // intentionally left blank
     "url": "http://csun.edu"
   }],
   "onlineCourses": [{
@@ -107,155 +108,245 @@ $("#body").toggleClass(HTMLcontainer, true);
 
 // Format the navigation
 nav.display = function () {
-  $("#nav").toggleClass(HTMLrow, true);
+  $("#nav").addClass(HTMLrow);
   $("#nav").append([HTMLtabs]);
   $("#tabs").append([HTMLtabList]);
   $(".tabList:last").append([HTMLnavProfile]);
   $(".tabList:last").append([HTMLnavWork]);
   $(".tabList:last").append([HTMLnavEducation]);
   $(".tabList:last").append([HTMLnavContact]);
-}
+};
 
 nav.display();
 
 // Format the header
 bio.displayHeader = function () {
-  $("#header").toggleClass(HTMLrow, true);
-  $("#header").append([HTMLheaderCol]);
-  $("#headerCol").append([HTMLbioPic.replace("%data%", bio.bioPic)]);
-  $("#headerCol").append([HTMLheaderName.replace("%data%", bio.name)]);
-  $("#headerCol").append([HTMLheaderRole.replace("%data%", bio.role)]);
-  $("#headerCol").append(
+  // Format the row and columns
+  $("#header").addClass(HTMLrow);
+  $("#header").append([HTMLpicCol]);
+  $("#header").append([HTMLbioCol]);
+  // Format the content
+  $("#picCol").append([HTMLbioPic.replace("%data%", bio.bioPic)]);
+  $("#bioCol").append([HTMLheaderName.replace("%data%", bio.name)]);
+  $("#bioCol").append([HTMLheaderRole.replace("%data%", bio.role)]);
+  $("#bioCol").append(
     [HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage)]);
-  $("#headerCol").append([HTMLrule]);
-}
+  $("#bioCol").append([HTMLrule]);
+};
 
 bio.displayHeader();
 
 // Format the skills
 bio.displaySkills = function () {
   if (bio.skills) {
-    $("#skills").toggleClass(HTMLrow, true);
-    $("#skills").append([HTMLskillsCol]);
-    bio.skills.forEach(function (skill) {
-      $("#skillsCol").append([HTMLskill.replace("%data%", skill)]);
-    })
+    // Format the row columns
+    $("#skills").addClass(HTMLrow);
+    $("#skills").append([HTMLskillsHeadingCol]);
+    $("#skillsHeadingCol").append([HTMLskillsCol1]);
+    $("#skillsHeadingCol").append([HTMLskillsCol2]);
+    // Format the content
+    bio.skills.forEach(function (skill, iSkill) {
+      if (!(iSkill % 2)) {
+        $("#skillsCol1").append([HTMLskill.replace("%data%", skill)]);
+      } else {
+        $("#skillsCol2").append([HTMLskill.replace("%data%", skill)]);
+      }
+    });
   }
-}
+};
 
 bio.displaySkills();
 
 // Format the work
 work.display = function () {
-  $("#workExperience").toggleClass(HTMLrow, true);
-  $("#workExperience").append([HTMLworkCol]);
+  // Format the row and heading
+  $("#workExperience").addClass(HTMLrow);
+  $("#workExperience").append([HTMLworkHeadingCol]);
+  // Format the content
   work.jobs.forEach(function (job) {
-    $("#workCol").append([HTMLworkStart]);
-    $(".work-entry:last").append([HTMLworkIcon]);
-    $(".work-entry:last").append(
+    // Format the work entry columns
+    $("#workExperience").append([HTMLworkStart]);
+    $(".work-entry:last").append([HTMLworkDatesCol]);
+    $(".work-entry:last").append([HTMLworkIconCol]);
+    $(".work-entry:last").append([HTMLworkTitleCol]);
+    $(".work-entry:last").append([HTMLworkCompanyCol]);
+    $(".work-entry:last").append([HTMLworkDescriptionCol]);
+    // Format the work entry content
+    $(".work-entry:last .workDatesCol").append(
+      [HTMLworkDates.replace("%data%", job.dates)]);
+    $(".work-entry:last .workIconCol").append([HTMLworkIcon]);
+    $(".work-entry:last .workTitleCol").append(
+      [HTMLworkTitle.replace("%data%", job.title)]);
+    $(".work-entry:last .workCompanyCol").append(
       [HTMLworkEmployer.replace("%data%", job.employer)]);
-    $(".work-entry:last").append([HTMLworkTitle.replace("%data%", job.title)]);
-    $(".work-entry:last").append([HTMLworkDates.replace("%data%", job.dates)]);
-    $(".work-entry:last").append(
+    $(".work-entry:last .workCompanyCol").append(
       [HTMLworkLocation.replace("%data%", job.location)]);
-    $(".work-entry:last").append(
+    $(".work-entry:last .workDescriptionCol").append(
       [HTMLworkDescription.replace("%data%", job.description)]);
-  })
-}
+  });
+};
 
 work.display();
 
 // Format the projects
 projects.display = function () {
-  $("#projects").toggleClass(HTMLrow, true);
-  $("#projects").append([HTMLprojectCol]);
+  // Format the row and heading
+  $("#projects").addClass(HTMLrow);
+  $("#projects").append([HTMLprojectHeadingCol]);
   projects.projects.forEach(function (project) {
-    $("#projectCol").append([HTMLprojectStart]);
-    $(".project-entry:last").append(
-      [HTMLprojectTitle.replace("%data%", project.title)]);
-    $(".project-entry:last").append(
+    // Format the project entry columns
+    $("#projects").append([HTMLprojectStart]);
+    $(".project-entry:last").append([HTMLprojectDateCol]);
+    $(".project-entry:last").append([HTMLprojectIconCol]);
+    $(".project-entry:last").append([HTMLprojectTitleCol]);
+    $(".project-entry:last").append([HTMLprojectDescriptionCol]);
+    $(".project-entry:last").append([HTMLprojectPicturesCol]);
+    // Format the content
+    $(".project-entry:last .projectDateCol").append(
       [HTMLprojectDates.replace("%data%", project.dates)]);
-    $(".project-entry:last").append(
+    $(".project-entry:last .projectIconCol").append([HTMLprojectIcon]);
+    $(".project-entry:last .projectTitleCol").append(
+      [HTMLprojectTitle.replace("%data%", project.title)]);
+    $(".project-entry:last .projectDescriptionCol").append(
       [HTMLprojectDescription.replace("%data%", project.description)]);
     project.images.forEach(function (image) {
-      $(".project-entry:last").append(
+      $(".project-entry:last .projectPicturesCol").append(
         [HTMLprojectImage.replace("%data%", image)]);
-    })
-  })
-}
+    });
+  });
+};
 
 projects.display();
 
 // Format the education
 education.display = function () {
-  $("#education").toggleClass(HTMLrow, true);
-  $("#education").append([HTMLschoolsCol]);
 
+  // Format the schools
+  // Format the row and heading
+  $("#education").addClass(HTMLrow);
+  $("#education").append([HTMLschoolsHeadingCol]);
   education.schools.forEach(function (school) {
-    $("#educationCol").append([HTMLschoolStart]);
-    $(".education-entry:last").append(
-      [HTMLschoolName.replace("%data%", school.name)]);
-    $(".education-entry:last").append(
-      [HTMLschoolLocation.replace(
-        "%data%", school.location)]);
-    $(".education-entry:last").append(
-      [HTMLschoolDegree.replace("%data%", school.degree)]);
-
-    school.majors.forEach(function (major) {
-      $(".education-entry:last").append(
-        [HTMLschoolMajor.replace("%data%", major)]);
-    })
-
-    $(".education-entry:last").append(
+    // Format the school entry columns
+    $("#education").append([HTMLschoolStart]);
+    $(".education-entry:last").append([HTMLschoolDatesCol]);
+    $(".education-entry:last").append([HTMLschoolIconCol]);
+    $(".education-entry:last").append([HTMLschoolNameCol]);
+    $(".education-entry:last").append([HTMLschoolLocationCol]);
+    $(".education-entry:last").append([HTMLschoolDegreeCol]);
+    // Format the content
+    $(".education-entry:last .schoolDatesCol").append(
       [HTMLschoolDates.replace("%data%", school.dates)]);
-    $(".education-entry:last").append(
-      [HTMLonlineURL.replace("%data%", school.url)]);
-  })
+    $(".education-entry:last .schoolIconCol").append([HTMLschoolIcon]);
+    $(".education-entry:last .schoolNameCol").append(
+      [HTMLschoolName.replace("%data%", school.name)]);
+    $(".education-entry:last .schoolLocationCol").append(
+      [HTMLschoolLocation.replace("%data%", school.location)]);
+    $(".education-entry:last .schoolLocationCol").append(
+      [HTMLschoolURL.replace("%data%", school.url)]);
+    $(".education-entry:last .schoolDegreeCol").append(
+      [HTMLschoolDegree.replace("%data%", school.degree)]);
+    school.majors.forEach(function (major) {
+      $(".education-entry:last .schoolDegreeCol").append(
+        [HTMLschoolMajor.replace("%data%", major)]);
+    });
+  });
 
   // Format the online courses
-  $(".education-entry:last").append([HTMLonlineClasses]);
-
+  // Format the heading
+  $("#education").append([HTMLcoursesHeadingCol]);
   education.onlineCourses.forEach(
     function (course) {
-      $(".education-entry:last").append(
-        [HTMLonlineTitle.replace("%data%", course.title)]);
-      $(".education-entry:last").append(
-        [HTMLonlineSchool.replace("%data%", course.school)]);
-      $(".education-entry:last").append(
-        [HTMLonlineDates.replace("%data%", course.dates)]);
-      $(".education-entry:last").append(
-        [HTMLonlineURL.replace("%data%", course.url)]);
-    })
-}
+      // Format the course entry columns
+      $("#education").append([HTMLcourseStart]);
+      $(".course-entry:last").append([HTMLcourseDatesCol]);
+      $(".course-entry:last").append([HTMLcourseIconCol]);
+      $(".course-entry:last").append([HTMLcourseTitleCol]);
+      $(".course-entry:last").append([HTMLcourseSchoolCol]);
+      // Format the content
+      $(".course-entry:last .courseDatesCol").append(
+        [HTMLcourseDates.replace("%data%", course.dates)]);
+      $(".course-entry:last .courseIconCol").append([HTMLcourseIcon]);
+      $(".course-entry:last .courseTitleCol").append(
+        [HTMLcourseTitle.replace("%data%", course.title)]);
+      $(".course-entry:last .courseSchoolCol").append(
+        [HTMLcourseSchool.replace("%data%", course.school)]);
+      $(".course-entry:last .courseSchoolCol").append(
+        [HTMLcourseURL.replace("%data%", course.url)]);
+    });
+};
+
 education.display();
 
 // Format the contacts
-$("#contacts").toggleClass(HTMLrow, true);
-$("#contacts").append([HTMLcontactsCol]);
 bio.displayContacts = function () {
-  $("#contactsCol").append([HTMLmobile.replace("%data%", bio.contacts.mobile)]);
-  $("#contactsCol").append([HTMLemail.replace("%data%", bio.contacts.email)]);
-  $("#contactsCol").append([HTMLgithub.replace("%data%", bio.contacts.github)]);
-  $("#contactsCol").append(
-    [HTMLtwitter.replace("%data%", bio.contacts.twitter)]);
-  $("#contactsCol").append([HTMLlocation.replace("%data%", bio.contacts.location)]);
-}
+  // Format the row and columns
+  $("#contacts").addClass(HTMLrow);
+  $("#contacts").append([HTMLcontactHeadingCol]);
+
+  // Format the contact row, column and content
+  $("#contactHeadingCol").append([HTMLcontactRow]);
+  $(".contact-row:last").append([HTMLcontactStart]);
+  $(".contact-entry:last").append([HTMLcontactBlankCol]);
+  $(".contact-entry:last").append([HTMLcontactIconCol]);
+  $(".contact-entry:last").append([HTMLcontactEntryCol]);
+
+  $(".contact-entry:last .contact-icon-col").append([HTMLmobileIcon]);
+  $(".contact-entry:last .contact-entry-col").append([HTMLcontact.replace("%data%", bio.contacts.mobile)]);
+
+  $(".contact-row:last").append([HTMLcontactStart]);
+  $(".contact-entry:last").append([HTMLcontactIconCol]);
+  $(".contact-entry:last").append([HTMLcontactEntryCol]);
+
+  $(".contact-entry:last .contact-icon-col").append([HTMLemailIcon]);
+  $(".contact-entry:last .contact-entry-col").append([HTMLcontact.replace("%data%", bio.contacts.email)]);
+
+  // Format the contact row, column and content
+  $("#contactHeadingCol").append([HTMLcontactRow]);
+  $(".contact-row:last").append([HTMLcontactStart]);
+  $(".contact-entry:last").append([HTMLcontactBlankCol]);
+  $(".contact-entry:last").append([HTMLcontactIconCol]);
+  $(".contact-entry:last").append([HTMLcontactEntryCol]);
+
+  $(".contact-entry:last .contact-icon-col").append([HTMLtwitterIcon]);
+  $(".contact-entry:last .contact-entry-col").append([HTMLcontact.replace("%data%", bio.contacts.twitter)]);
+
+  $(".contact-row:last").append([HTMLcontactStart]);
+  $(".contact-entry:last").append([HTMLcontactIconCol]);
+  $(".contact-entry:last").append([HTMLcontactEntryCol]);
+
+  $(".contact-entry:last .contact-icon-col").append([HTMLgithubIcon]);
+  $(".contact-entry:last .contact-entry-col").append([HTMLcontact.replace("%data%", bio.contacts.github)]);
+
+  // Format the contact row, column and content
+  $("#contactHeadingCol").append([HTMLcontactRow]);
+  $(".contact-row:last").append([HTMLcontactStart]);
+  $(".contact-entry:last").append([HTMLcontactBlankCol]);
+  $(".contact-entry:last").append([HTMLcontactIconCol]);
+  $(".contact-entry:last").append([HTMLcontactEntryCol]);
+
+  $(".contact-entry:last .contact-icon-col").append([HTMLblogIcon]);
+  $(".contact-entry:last .contact-entry-col").append([HTMLcontact.replace("%data%", bio.contacts.blog)]);
+
+  $(".contact-row:last").append([HTMLcontactStart]);
+  $(".contact-entry:last").append([HTMLcontactIconCol]);
+  $(".contact-entry:last").append([HTMLcontactEntryCol]);
+
+  $(".contact-entry:last .contact-icon-col").append([HTMLlocationIcon]);
+  $(".contact-entry:last .contact-entry-col").append([HTMLcontact.replace("%data%", bio.contacts.location)]);
+};
 
 bio.displayContacts();
 
 // Format the map
-$("#theMap").toggleClass(HTMLrow, true);
+$("#theMap").addClass(HTMLrow);
 $("#theMap").append([HTMLmapCol]);
 $("#mapDiv").append([HTMLmapHeading]);
 $("#mapDiv").append(googleMap);
 
-// Turn on the click tracking
-$(document).click(function (loc) {
-  logClicks(event.pageX, event.pageY);
-});
-
 // Format the internationalization of the name
-$("#main").append([internationalizeButton]);
+$("#lets-connect").addClass(HTMLrow);
+$("#lets-connect").append([HTMLintlCol]);
+$("#intlCol").append([intlButton]);
 
 function inName(myName) {
   myName = myName.trim().split(" ");
@@ -266,3 +357,8 @@ function inName(myName) {
     myName[1].toLocaleUpperCase();
   return bio.name;
 }
+
+// Turn on the click tracking
+$(document).click(function (loc) {
+  logClicks(event.pageX, event.pageY);
+});
